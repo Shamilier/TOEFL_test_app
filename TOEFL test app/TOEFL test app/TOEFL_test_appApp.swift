f3fa2b1dac3ad81516d08e4614f4ed07aa9744d6
Shamil <shamilgaliev18mail.ru@MacBook-Pro-Shamil.local>
2025-12-16 16:44:32 +0300

    init

---
+++ TOEFL test app/TOEFL test app/TOEFL_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOEFL_test_appApp.swift
+//  TOEFL test app
+//
+//  Created by Shamil on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TOEFL_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TOEFL test app/TOEFL test appTests/TOEFL_test_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  TOEFL_test_appTests.swift
+//  TOEFL test appTests
+//
+//  Created by Shamil on 16.12.2025.
+//
+
+import Testing
+@testable import TOEFL_test_app
+
+struct TOEFL_test_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
